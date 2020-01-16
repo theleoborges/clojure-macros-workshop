58705920d1d1772c64c7dd752ced97da10de469d
Leonardo Borges <leonardoborges.rj@gmail.com>
2020-01-16 10:56:24 -0800

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [korma "0.3.0-RC5"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]
-                 [inflections "0.8.1"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [korma "0.4.3"]
+                 [org.xerial/sqlite-jdbc "3.30.1"]
+                 [inflections "0.13.2"]]
   :profiles {:dev
-             {:dependencies [[midje "1.5.1"]]
-              :plugins [[lein-midje "3.0.1"]]}}
+             {:dependencies [[midje "1.9.9"]]
+              :plugins [[lein-midje "3.2.2"]]}}
   :local-repo "repository")
